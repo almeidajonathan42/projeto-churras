f000e665b1ea934a1a89e5e0e46a33c27157fd5c
Jonathan Rocha <jonathan.rocha@msn.com>
2023-05-15 23:55:48 -0300

    Create NewEventDialog component

---
+++ src/components/NewEventDialog.js
@@ -0,0 +1,23 @@
+
+import bbqIconCircle from "../assets/bbqIconCircle.svg";
+import { useState } from "react";
+
+function NewEventDialog() {
+
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        zIndex: "100",
+        background: "blue"
+      }}
+    >
+    </div>
+  );
+}
+
+export default NewEventDialog;
